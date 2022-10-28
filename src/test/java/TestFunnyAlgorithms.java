320b171dbd77cc431b76c148b4019669efe1b0fa
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2022-10-28 16:51:58 +0200

    Added tests for selectionSort and binarySearch

--- src/test/java/TestFunnyAlgorithms.java
+++ src/test/java/TestFunnyAlgorithms.java
@@ -1,5 +1,8 @@
 import org.example.FunnyAlgorithms;
+import org.joda.time.DateTime;
 import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestFunnyAlgorithms {
 
@@ -10,6 +13,13 @@ public class TestFunnyAlgorithms {
     static String stringOutOfBound;
     static String stringCorrect;
 
+    static FunnyAlgorithms funnyAlgorithms;
+    static int[] array;
+    static int[] sortedArray;
+
+    static int target;
+    static int nonExistingTarget;
+
     @BeforeClass
     public static void Initialize() {
         stringWithDash = "-329";
@@ -17,8 +27,25 @@ public class TestFunnyAlgorithms {
         stringWithSpecialCharacters = "34_3";
         stringOutOfBound = "2938744";
         stringCorrect = "436";
+
+        funnyAlgorithms = new FunnyAlgorithms();
+
+        array = new int[]{10, 30, 2, 6};
+        sortedArray = new int[]{2, 6, 10, 30};
+
+        target = 2;
+        nonExistingTarget = 23;
     }
 
+    @Before
+    public void printStartTestTime() {
+        System.out.println("\nTest start time: " + DateTime.now());
+    }
+
+    @After
+    public void printEndTestTime() {
+        System.out.println("Test end time: " + DateTime.now());
+    }
     @Test()
     public void testStringWithDash() {
         System.out.println("Testing string with dash");
@@ -51,5 +78,33 @@ public class TestFunnyAlgorithms {
     public void testCorrectString() {
         System.out.println("Testing correct string");
         FunnyAlgorithms.stringToIntConverter(stringCorrect);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSelectionSortArguments() {
+        System.out.println("Testing selection sort arguments");
+        funnyAlgorithms.selectionSort(array, -1);
+    }
+
+    @Test
+    public void testSelectionSortSorting() {
+        System.out.println("Testing selection sort sorting");
+
+        funnyAlgorithms.selectionSort(array, 1);
+        Assert.assertArrayEquals(array, sortedArray);
+    }
+
+    @Test
+    public void testBinarySearchTargetExistance() {
+        System.out.println("Testing binary search");
+
+        Assert.assertEquals(2, target);
+    }
+
+    @Test
+    public void testBinarySearchTargetNonexistence() {
+        System.out.println("Testing binary search");
+
+        Assert.assertEquals(-1, funnyAlgorithms.binarySearch(array, nonExistingTarget));
     }
 }
