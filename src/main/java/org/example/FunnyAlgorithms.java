56c723a5567f5969f42325f1144461ada2fbbd8a
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2022-10-28 15:25:31 +0200

    Created FunnyAlgorithms class

---
+++ src/main/java/org/example/FunnyAlgorithms.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class FunnyAlgorithms {
+
+    public static Integer stringToIntConverter(String string) {
+
+        return Integer.parseInt(string);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+    String testString = "923487";
 
+    System.out.println(FunnyAlgorithms.stringToIntConverter(testString));
 
     }
 }
